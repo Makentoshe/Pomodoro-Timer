aa072bcf4eb2ce48751ea209053a07f4f560fad7
Makentoshe <mkliverout@gmail.com>
2020-10-24 20:51:30 +0300

    refactor(stage 2): Update AndroidEduTestRunner

--- Pomodoro Timer/stage2/src/androidTest/java/com/edu/AndroidEduTestRunner.kt
+++ Pomodoro Timer/stage2/src/androidTest/java/com/edu/AndroidEduTestRunner.kt
@@ -3,20 +3,16 @@ package com.edu
 import androidx.test.runner.AndroidJUnitRunner
 
 class AndroidEduTestRunner : AndroidJUnitRunner() {
+
     override fun sendStatus(resultCode: Int, results: android.os.Bundle) {
-        testNum++
         if (resultCode < 0) {
             val stack = results.getString("stack")
+            val testTitle = results.get("test")
             if (stack != null) {
                 val errorMessage = stack.substringAfter(":").trim().substringBefore("\n")
-                results.putString("stack", "#educational_plugin FAILED + " +
-                        "Wrong answer in test #${testNum / 2} $errorMessage")
+                results.putString("stack", "#educational_plugin FAILED + Wrong answer in test $testTitle: $errorMessage")
             }
         }
         super.sendStatus(resultCode, results)
-    }
-
-    companion object {
-        var testNum = 0
     }
 }
